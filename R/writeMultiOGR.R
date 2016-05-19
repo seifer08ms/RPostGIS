ef09c6a17f3ce0ce4a75a72881ffd0f09b59e114
seifer08ms <hi181904665@gmail.com>
2016-05-19 22:21:48 +0800

    add encode option for writeMultiOGR

--- R/writeMultiOGR.R
+++ R/writeMultiOGR.R
@@ -7,21 +7,26 @@
 #' @param host If shp2pgsql is true,a charater is required to indicate hostname or ip address of PostgreSQL Server
 #' @param username If shp2pgsql is true,a charater is required to indicate current user
 #' @param verbose a boolean variable indicating whether to display verbose information
+#' @param  encode a character indicating encode of vector data
 #' @return Spatial DataFrame
 #' @keywords writeMultiOGR
 #' @export
 
 writeMultiOGR<-function(x,dsn,nln=' ',verbose=F,srid='4326',dbname='mydb',host=' ',username=' ',
-                        simplify = T,shp2pgsql=F)
+                        simplify = T,shp2pgsql=F,encode=NULL)
 {
     require(gdalUtils)
     require(rgdal)
     tmpdsn<-file.path(tempdir(),'ttt_ogrm.shp')
     if (file.exists(tmpdsn)) {file.remove(tmpdsn)}
-    getCPLConfigOption("SHAPE_ENCODING")
-    Sys.getenv("SHAPE_ENCODING")
-    setCPLConfigOption("SHAPE_ENCODING", "UTF-8")
-    Sys.setenv(SHAPE_ENCODING= "UTF-8" )
+    if(!is.null(encode)){
+      getCPLConfigOption("SHAPE_ENCODING")
+      Sys.getenv("SHAPE_ENCODING")
+      # setCPLConfigOption("SHAPE_ENCODING", "UTF-8")
+      # Sys.setenv(SHAPE_ENCODING= "UTF-8" )
+      setCPLConfigOption("SHAPE_ENCODING", encode)
+      Sys.setenv(SHAPE_ENCODING= encode)
+    }
 
     #   if (file.exists(tmpdsn)) {file.remove(tmpdsn)}
     #  "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"  #srid=4326
